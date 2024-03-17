b0229fef3fc8759a91d72e10f1ed9b34b89c111d
LisitsaP <lisitsa.p.e@gmail.com>
2024-03-17 16:29:35 -0500

    map conversations on frontend side

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,13 +1,21 @@
+import useGetConversations from "../../hooks/useGetConversation";
+import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation";
 
 export default function Conversations() {
+  const { loading, conversations } = useGetConversations();
+
+  console.log("CONVERSATIONS:", conversations);
   return (
-    <div>
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+    <div className="py'2 flex flex-col overflow-auto">
+      {conversations.map((conversation, idx) => (
+        <Conversation
+          key={conversations._id}
+          conversation={conversation}
+          emoji={getRandomEmoji()}
+          lastIdx={idx === conversations.length - 1}
+        />
+      ))}
     </div>
   );
 }
---
+++ frontend/src/zustand/useConversation.js
@@ -0,0 +1,9 @@
+import {create} from 'zustand';
+
+const useConversation = create((set) =>({selectedConversation: null,
+     setSelectedConversation: (selectedConversation) =>set({selectedConversation}),
+messages:[],
+setMessges:[],
+setMessages: (messages) => set({messages}),
+}))
+export default useConversation;
